e5302ba1d6092925bb8ff5a20a0a34234b14af1b
Yi Zhen <quekyizhen@gmail.com>
2020-11-01 17:46:03 +0800

    changed styling of list of symptoms

--- src/components/SymptomsPage.vue
+++ src/components/SymptomsPage.vue
@@ -10,44 +10,62 @@
         </p>
 
         <h2 class="list">List of Symptoms: (Check all applicable)</h2>
-        <p class="symps">
+        
+
+            <div class="container"> 
         <img src="@/assets/fever.jpg" width="300" height="200" alt="Fever Image"><br>
         <input type="checkbox" value="Fever" v-model="symptomsSelected">
         <label for="fever">Fever</label> <br><br>
+            </div>
 
+            <div class="container"> 
         <img src="@/assets/cough.jpg" width="300" height="200" alt="Cough Image"><br>
         <input type="checkbox" value="Cough" v-model="symptomsSelected">
         <label for="cough">Cough</label> <br><br>
+            </div>
 
+            <div class="container"> 
         <img src="@/assets/chills.jpg" width="300" height="200" alt="Chills Image"><br>
         <input type="checkbox" value="Chills or rigors" v-model="symptomsSelected">
         <label for="chills">Chills or rigors</label> <br><br>
+            </div>
 
+            <div class="container"> 
         <img src="@/assets/sorethroat.jpg" width="300" height="200" alt="Sore Throat Image"><br>
         <input type="checkbox" value="Sore throat" v-model="symptomsSelected">
         <label for="sore">Sore throat</label> <br><br>
+            </div>
 
+            <div class="container"> 
         <img src="@/assets/breath.jpg" width="300" height="200" alt="Shortness of Breath Image"><br>
         <input type="checkbox" value="Shortness of breath" v-model="symptomsSelected">
         <label for="short">Shortness of breath</label> <br><br>
+            </div>
 
+            <div class="container"> 
         <img src="@/assets/runnynose.jpg" width="300" height="200" alt="Runny Nose Image"><br>
         <input type="checkbox" value="Runny nose" v-model="symptomsSelected">
         <label for="runny">Runny nose</label> <br><br>
+            </div>
 
+            <div class="container"> 
         <img src="@/assets/headache.jpg" width="300" height="200" alt="Headache Image"><br>
         <input type="checkbox" value="Headache" v-model="symptomsSelected">
         <label for="headache">Headache</label> <br><br>
+            </div>
 
+            <div class="container"> 
         <img src="@/assets/fatigue.jpg" width="300" height="200" alt="Fatigue Image"><br>
         <input type="checkbox" value="Fatigue" v-model="symptomsSelected">
         <label for="fatigue">Fatigue</label> <br><br>
+            </div>
 
+            <div class="container"> 
         <img src="@/assets/lossoftaste.jpg" width="300" height="200" alt="Loss of Taste/Smell Image"><br>
         <input type="checkbox" value="Loss of taste/smell" v-model="symptomsSelected">
         <label for="loss">Loss of taste/smell</label> <br><br>
-        </p>
-        
+            </div>
+      
         <p class="symps" v-if="symptomsSelected.length !=0"> Symptom(s) Selected: 
              <ul>
                 <li v-for="symps in symptomsSelected" v-bind:key="symps">
@@ -183,6 +201,12 @@ export default {
 }
 .title {
   text-align: center;
+}
+
+.container {
+    text-align: left;
+    display: inline-block;
+    padding: 40px;
 }
 .list {
   text-align: left;
