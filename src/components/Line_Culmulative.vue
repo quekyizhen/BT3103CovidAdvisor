d1945b28f09afce30f2c7e38fc0302ed6e9e1581
Jed <16scjed@gmail.com>
2020-11-12 18:33:18 +0800

    Added New Chart (Cul)

---
+++ src/components/Line_Culmulative.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="myChart">
+    <chart></chart>
+  </div>
+</template>
+
+<script>
+import Chart from "./Line_Culmulative.js";
+export default {
+  components: {
+    Chart,
+  },
+};
+</script>
+
+<style></style>
